56ea422877a328cd22d684da685ee9fd9a33c0ec
Alex Chipre <alechipre@gmail.com>
2023-11-01 15:57:03 -0500

    Finish Challenge

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,21 @@ import Logo from './assets/logo.svg'
 import Arrow from './assets/icon-arrow.svg'
 import HeroMovile from './assets/hero-mobile.jpg'
 import HeroDesktop from './assets/hero-desktop.jpg'
+import AlertIcon from './assets/icon-error.svg'
 import './App.css'
+import { useForm } from 'react-hook-form'
+import { useState } from 'react'
 
 export const App = () => {
+  const [valid, setValid] = useState(false)
+  const { register, handleSubmit, reset, formState: { errors } } = useForm()
+  const onSubmit = () => {
+    setValid(true)
+    reset()
+  }
+  const pressKey = () => {
+    setValid(false)
+  }
   return (
     <main>
       <aside className="aside-left">
@@ -18,9 +30,31 @@ export const App = () => {
           Hello fellow shoppers! We're currently building our new fashion store. Add your email
           below to stay up-to-date with announcements and our launch deals.
         </p>
-        <form className='input-cover'>
-          <input type="email" placeholder="Email Address" />
-          <button><img src={Arrow} alt="Icono de Siguiente" /></button>
+        <form className="input-cover" onSubmit={handleSubmit(onSubmit)}>
+          <input
+            onKeyDown={pressKey}
+            placeholder="Email Address"
+            {...register('email', {
+              required: true,
+              pattern: /^[\w-]+(\.[\w-]+)*@[\w-]+(\.[\w-]+)*(\.[a-z]{2,})$/
+            })}
+          />
+          {errors?.email?.type === 'required' && (
+            <>
+              <p>Please, cannot be empty</p>
+              <img className="icon-allert" src={AlertIcon} alt="Icono de Alerta" />
+            </>
+          )}
+          {errors?.email?.type === 'pattern' && (
+            <>
+              <p>Please provide a valid email</p>
+              <img className="icon-allert" src={AlertIcon} alt="Icono de Alerta" />
+            </>
+          )}
+          {valid && <p className='success'>Tanks for suscribers!</p>}
+          <button type="submit">
+            <img src={Arrow} alt="Icono de Siguiente" />
+          </button>
         </form>
       </aside>
       <aside className="aside-right">
